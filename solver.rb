1e4ea268d8dfb6e86fa39eb17a5ff24272828941
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2014-04-12 16:32:21 +0300

    add file input support for solver
    now loads an input file and asks the solver controller to parse the input once for every case
    it also calls the solve method with the parsed value

--- solver.rb
+++ solver.rb
@@ -1,5 +1,13 @@
 module Solver
-  def solve_with_file
-    solve
+  def solve_with_file (in_file)
+    File.open(in_file, 'r') do |input|
+      #File.open(in_file.sub(/\.in/, '.out'), 'w') do |output|
+      cases = input.readline.to_i
+      cases.times do
+        test_data = parse(input)
+        solve(test_data)
+      end
+      #end
+    end
   end
 end
--- solverSpec.rb
+++ solverSpec.rb
@@ -4,9 +4,52 @@ describe Solver do
   let(:solver) { Class.new { extend Solver } }
 
   describe '#solve_with_file' do
-    it 'should call solve' do
-      solver.should_receive(:solve)
-      solver.solve_with_file
+    let(:filename) { 'filename.in' }
+    before(:each) do
+      solver.stub(:solve)
+      solver.stub(:parse)
+    end
+
+    context 'with 1 case' do
+      before(:each) do
+        File.stub(:open).with(filename,'r').and_yield(StringIO.new("1\nb"))
+      end
+
+      it 'should call solve' do
+        solver.should_receive(:solve)
+        solver.solve_with_file(filename)
+      end
+
+      it 'should open file' do
+        File.should_receive(:open).with(filename, 'r')
+        solver.solve_with_file(filename)
+      end
+    end
+
+    context 'with 2 cases' do
+      let(:input) { "2\ncase1\ncase2" }
+      before(:each) do
+        File.stub(:open).with(filename,'r').and_yield(StringIO.new(input))
+      end
+
+      it 'should call parse for each case' do
+        solver.should_receive(:parse).twice
+        solver.solve_with_file(filename)
+      end
+
+      context 'with readline parser' do
+        before(:each) do
+          solver.stub(:parse) do |input|
+            input.readline.sub(/\n/, '')
+          end
+        end
+
+        it 'should call solver with lines read from input' do
+          solver.should_receive(:solve).with('case1')
+          solver.should_receive(:solve).with('case2')
+          solver.solve_with_file(filename)
+        end
+      end
     end
   end
 end
