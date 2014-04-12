8ace666f7958a93cf91ae52786f343a0fdda1487
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2014-04-12 19:49:12 +0300

    make solver module write to out file

--- solver.rb
+++ solver.rb
@@ -1,13 +1,16 @@
 module Solver
   def solve_with_file (in_file)
     File.open(in_file, 'r') do |input|
-      #File.open(in_file.sub(/\.in/, '.out'), 'w') do |output|
-      cases = input.readline.to_i
-      cases.times do
-        test_data = parse(input)
-        solve(test_data)
+      File.open(in_file.sub(/\.in/, '.out'), 'w') do |output|
+        cases = input.readline.to_i
+        1.upto(cases).each do |case_nr|
+          output.write("Case ##{case_nr}: ")
+          test_data = parse(input)
+          solution = solve(test_data)
+          dump(output, solution)
+          output.write("\n") if case_nr != cases
+        end
       end
-      #end
     end
   end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -5,14 +5,17 @@ describe Solver do
 
   describe '#solve_with_file' do
     let(:filename) { 'filename.in' }
+    let(:out_filename) { 'filename.out' }
     before(:each) do
       solver.stub(:solve)
       solver.stub(:parse)
+      solver.stub(:dump)
     end
 
     context 'with 1 case' do
       before(:each) do
-        File.stub(:open).with(filename,'r').and_yield(StringIO.new("1\nb"))
+        allow(File).to receive(:open).with(filename,'r').and_yield(StringIO.new("1\nb"))
+        allow(File).to receive(:open).with(out_filename,'w').and_yield(StringIO.new)
       end
 
       it 'should call solve' do
@@ -20,8 +23,13 @@ describe Solver do
         solver.solve_with_file(filename)
       end
 
-      it 'should open file' do
+      it 'should open input file' do
         File.should_receive(:open).with(filename, 'r')
+        solver.solve_with_file(filename)
+      end
+
+      it 'should open ouput file' do
+        File.should_receive(:open).with(out_filename, 'w')
         solver.solve_with_file(filename)
       end
     end
@@ -29,7 +37,9 @@ describe Solver do
     context 'with 2 cases' do
       let(:input) { "2\ncase1\ncase2" }
       before(:each) do
-        File.stub(:open).with(filename,'r').and_yield(StringIO.new(input))
+        @ouput = StringIO.new
+        allow(File).to receive(:open).with(filename,'r').and_yield(StringIO.new(input))
+        allow(File).to receive(:open).with(out_filename,'w').and_yield(@ouput)
       end
 
       it 'should call parse for each case' do
@@ -48,6 +58,32 @@ describe Solver do
           solver.should_receive(:solve).with('case1')
           solver.should_receive(:solve).with('case2')
           solver.solve_with_file(filename)
+        end
+      end
+
+      context 'dummy solver responses' do
+        before(:each) do
+          allow(solver).to receive(:solve).and_return('solution1', 'solution2')
+        end
+
+        it 'should call dump with ouput and solutions from solver' do
+          solver.should_receive(:dump).with(@ouput, 'solution1')
+          solver.should_receive(:dump).with(@ouput, 'solution2')
+          solver.solve_with_file(filename)
+        end
+      end
+
+      context 'dummy dump implementation' do
+        before(:each) do
+          allow(solver).to receive(:solve).and_return('solution1', 'solution2')
+          allow(solver).to receive(:dump) do |output, solution|
+            output.write(solution)
+          end
+        end
+
+        it 'write results to output file' do
+          solver.solve_with_file(filename)
+          expect(@ouput.string).to eq("Case #1: solution1\nCase #2: solution2")
         end
       end
     end
