3ab07eb7104c521f70354d6fc9bd162c2cd60ade
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2014-05-03 20:43:21 +0300

    solve repeater

---
+++ repeater/repeater.rb
@@ -0,0 +1,61 @@
+require './solver'
+
+class Repeater
+  include Solver
+
+  def solve(input)
+    heat_maps = input.map(&method(:get_heat_map_for_string))
+    return nil unless same_keys(heat_maps)
+    nr_letters = heat_maps.first.length
+    nr_strings = heat_maps.length
+    sum = 0
+    for letter_i in 0..(nr_letters -1)
+      letter_sum = 0
+      heat_maps.each { |heat_map| letter_sum += heat_map[letter_i][1] }
+      letter_average = letter_sum / nr_strings
+      heat_maps.each { |heat_map| sum += (letter_average - heat_map[letter_i][1]).abs }
+    end
+    sum
+  end
+
+  def dump(output, solution)
+    if solution.nil?
+      output.write('Fegla Won')
+    else
+      output.write(solution)
+    end
+  end
+
+  def parse(input)
+    nr_strings = input.readline.to_i
+    result = []
+    nr_strings.times do
+      result << input.readline.strip
+    end
+    return result
+  end
+
+  private
+  def get_heat_map_for_string(s)
+    s.split(//).chunk(&:to_s).map do |key, value|
+      [key, value.length]
+    end
+  end
+
+  def same_keys(arr)
+    first_keys = nil
+    arr.all? do |element|
+      if first_keys
+        element.length == first_keys.length &&
+        get_letters_for_heat_map(element) == first_keys
+      else
+        first_keys = get_letters_for_heat_map(element)
+        true
+      end
+    end
+  end
+
+  def get_letters_for_heat_map(heat_map)
+    heat_map.map { |heat| heat.first }
+  end
+end
---
+++ spec/repeater_spec.rb
@@ -0,0 +1,149 @@
+require './repeater/repeater'
+
+describe Repeater do
+  let(:solver) {Repeater.new}
+
+  describe '#solve' do
+    it 'should solve sample 1' do
+      input = ['mmaw', 'maw']
+
+      solution = solver.solve(input)
+      expect(solution).to eq(1)
+    end
+
+    it 'should solve sample 2' do
+      input = ['gcj', 'cj']
+
+      solution = solver.solve(input)
+      expect(solution).to eq(nil)
+    end
+
+    it 'should solve sample 3' do
+      input = ['aaabbb','ab','aabb']
+
+      solution = solver.solve(input)
+      expect(solution).to eq(4)
+    end
+
+    it 'should solve sample 4' do
+      input = ['abc', 'abc']
+
+      solution = solver.solve(input)
+      expect(solution).to eq(0)
+    end
+
+    it 'should solve sample 5' do
+      input = ['aabc', 'abbc', 'abcc']
+
+      solution = solver.solve(input)
+      expect(solution).to eq(3)
+    end
+  end
+
+  describe '#parse' do
+    let(:input_file) {"repeater/sample.in"}
+    it 'should return array with 2 strings' do
+      File.open(input_file, 'r') do |input|
+        input.readline
+        result = solver.parse(input)
+        expect(result.length).to eq(2)
+        expect(result).to include("mmaw")
+        expect(result).to include("maw")
+      end
+    end
+  end
+
+  describe '#dump' do
+    before(:each) do
+      @output = StringIO.new
+    end
+
+    it 'should write number if result has one' do
+      @output.should_receive(:write).with(7)
+      solver.dump(@output, 7)
+    end
+
+    it 'should write number even if 0' do
+      @output.should_receive(:write).with(0)
+      solver.dump(@output, 0)
+    end
+
+    it 'should write fegla won if nil' do
+      @output.should_receive(:write).with('Fegla Won')
+      solver.dump(@output, nil)
+    end
+  end
+
+  describe '#get_heat_map_for_string' do
+    it 'should return number of occurences for each letter of a string' do
+      result = solver.send(:get_heat_map_for_string, 'acccbb')
+      result.should eql([
+        ['a', 1],
+        ['c', 3],
+        ['b', 2]
+        ])
+    end
+
+    it 'should allow same letter twice' do
+      result = solver.send(:get_heat_map_for_string, 'acccbbcccc')
+      result.should eql([
+        ['a', 1],
+        ['c', 3],
+        ['b', 2],
+        ['c', 4]
+        ])
+    end
+  end
+
+  describe '#same_keys' do
+    it 'should return true if all hashes have the same number of elements' do
+      result = solver.send(:same_keys, [[
+        ['a', 1],
+        ['c', 3],
+        ['b', 2]
+        ],[
+        ['a', 1],
+        ['c', 3],
+        ['b', 2]
+        ]])
+      result.should eql(true)
+    end
+
+    it 'should return false if hashes have different lengths' do
+      result = solver.send(:same_keys, [[
+        ['a', 1],
+        ['c', 3],
+        ['b', 2]
+        ],[
+        ['a', 1],
+        ['c', 3]
+        ]])
+      result.should eql(false)
+    end
+
+    it 'should return false if hashes have different keys, but same lengths' do
+      result = solver.send(:same_keys, [[
+        ['a', 1],
+        ['c', 3],
+        ['b', 2]
+        ],[
+        ['a', 1],
+        ['b', 2],
+        ['c', 3]
+        ]])
+      result.should eql(false)
+    end
+  end
+
+  describe '#get_letters_for_heat_map' do
+    it 'should return array of arrays first subelements' do
+      result = solver.send(:get_letters_for_heat_map, [
+        ['a', 1],
+        ['c', 3],
+        ['b', 2],
+        ['c', 2]
+        ])
+      result.should eql(['a', 'c', 'b', 'c'])
+    end
+  end
+end
